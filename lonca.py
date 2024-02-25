658eebd8b5deef4eee82bc6c7bbdc8b91401464c
EmreKomsal <komsal16@itu.edu.tr>
2024-02-25 13:08:47 +0300

    Update lonca.py

--- lonca.py
+++ lonca.py
@@ -70,6 +70,8 @@ class XMLParser:
                 fabric_info = self.extract_info(description_cdata, "Kumaş Bilgisi")     # Extract the fabric information
                 model_measurements = self.extract_info(description_cdata, "Model Ölçüleri")     # Extract the model measurements
                 product_measurements = self.extract_info(description_cdata, "Ürün Ölçüleri")       # Extract the product measurements
+                description_text = "Modelin üzerindeki ürün <strong>S/36</strong>&nbsp;bedendir."
+                sample_size = self.extract_sample_size(description_text)  # Extract the sample size
 
                 product_data = {
                     "stock_code": p.get("ProductId"),
@@ -77,12 +79,12 @@ class XMLParser:
                     "discounted_price": float(details.get("DiscountedPrice").replace(",", ".")) if details.get("DiscountedPrice") else None,
                     "images": images,
                     "is_discounted": details.get("DiscountedPrice") is not None and float(details.get("DiscountedPrice").replace(",", ".")) < float(details.get("Price").replace(",", ".")),
-                    "name": p.get("Name"),
+                    "name": p.get("Name").capitalize(),
                     "price": float(details.get("Price").replace(",", ".")),
                     "price_unit": "USD",  # Assuming this is constant
                     "product_type": details.get("ProductType"),
                     "quantity": int(details.get("Quantity")),
-                    "sample_size": None,  # Will get this from the XML later
+                    "sample_size": sample_size,  # Will get this from the XML later
                     "series": details.get("Series"),
                     "status": "Active",  # Assuming this is constant
                     "fabric": fabric_info,  # Will get this from the XML later
@@ -105,6 +107,14 @@ class XMLParser:
             return html.unescape(match.group(1).strip())    # Return the extracted information
         else:
             return None  # Return None if the pattern is not found
+    
+    def extract_sample_size(self, description):  # Extract the sample size from the description
+        pattern = r"<strong>(.*?)<\/strong>"  # Define the pattern to search for
+        match = re.search(pattern, description)     # Search for the pattern in the description
+        if match:   # If the pattern is found
+            return html.unescape(match.group(1).strip())    # Return the extracted sample size
+        else:   # If the pattern is not found
+            return None     # Return None
 
 class ProductImporter:
     def __init__(self, file_path, db_client): # Initialize the ProductImporter class
